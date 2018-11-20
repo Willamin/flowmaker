08e63393c292ab2e77b9a8166c85cf4e788c0c80
Will Lewis <will@wflewis.com>
2018-11-20 10:47:43 -0500

    :sparkles: initial formation

---
+++ spec/spec_helper.cr
@@ -0,0 +1,4 @@
+require "../src/flowmaker"
+require "microtest"
+include Microtest::DSL
+Microtest.run!
---
+++ src/flowmaker.cr
@@ -0,0 +1,3 @@
+module Flowmaker
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+end
