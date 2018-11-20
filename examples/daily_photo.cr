0f54c14f320e57d6d488fa9ea6924527e83b7a7e
Will Lewis <will@wflewis.com>
2018-11-20 14:03:03 -0500

    Auto wrap param values

--- examples/daily_photo.cr
+++ examples/daily_photo.cr
@@ -2,12 +2,11 @@ require "../src/flowmaker"
 
 wf = Flowmaker::Workflow.new
 wf.action("is.workflow.actions.getlastphoto") do
-  params["WFGetLatestPhotosActionIncludeScreenshots"] = "<false/>"
+  params["WFGetLatestPhotosActionIncludeScreenshots"] = false
 end
 
 wf.action("is.workflow.actions.savetocameraroll") do
-  params["UUID"] = "<string>EFF039EC-C0B8-45DA-A082-FA79A3C4F5DE</string>"
-  params["WFCameraRollSelectedGroup"] = "<string>Every Day</string>"
+  params["WFCameraRollSelectedGroup"] = "Every Day"
 end
 
 puts wf
